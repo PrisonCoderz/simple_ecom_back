cafd7a9bf8ac3da448994b50dde174518948646f
angrycoderz <angrycoderz01@gmail.com>
2023-02-10 00:56:59 +0500

    back

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config();
+// require('dotenv').config();
 require('./db/conn')
 
 const authRoutes = require('./routes/auth')
